8234bb5d8ba59a18899947a2a1418ddd756d0405
tim03bad <124298800+tim03bad@users.noreply.github.com>
2023-03-26 14:28:04 +0200

    Add files via upload
    mise à jour prototype avec indication

--- projet/proto.py
+++ projet/proto.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 from matplotlib.figure import Figure
 import yfinance as yf
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from fpdf import FPDF
 import os
@@ -16,37 +15,17 @@ historique = nouv.split()
 auj=datetime.date.today()
 Mem=[]
 
-def update() :
+def update() :                               #mise à jour du fichier historique à chaque menu déroulant
     fichier = open("historique.txt", "r")
     nouv = fichier.read()
     fichier.close()
     text["values"] = nouv.split()
 
-root = tk.Tk()
+root = tk.Tk()              #mise en place de l'interface
 root.geometry("1920x1080")
 root.configure(bg='grey')
 
-def alert():
-    tk.messagebox.showinfo("alerte", "Bravo!")
 
-menubar = tk.Menu(root)
-
-menu1 = tk.Menu(menubar, tearoff=0)
-menubar.add_cascade(label="Fichier", menu=menu1)
-
-menu2 = tk.Menu(menubar, tearoff=0)
-menu2.add_command(label="Couper", command=alert)
-menu2.add_command(label="Copier", command=alert)
-menu2.add_command(label="Coller", command=alert)
-menubar.add_cascade(label="Editer", menu=menu2)
-
-menu3 = tk.Menu(menubar, tearoff=0)
-menu3.add_command(label="A propos", command=alert)
-menubar.add_cascade(label="Aide", menu=menu3)
-
-root.config(menu=menubar)
-# textExample=tk.Label(root, text='Bonjour')
-# textExample.pack()
 var1 = tk.BooleanVar()
 check1 = tk.Checkbutton(root, text = "chandelier", height = 2, width = 10, variable=var1)
 check1.place(x=200,y=700)
@@ -57,14 +36,14 @@ fin=tk.Label(text="FIN :", fg="red", height=1, width=4, padx=5, bg='grey')
 text=tk.ttk.Combobox(root, values=historique, width=120, postcommand=update)
 text.place(x=135, y=24)
 
-ca1=tk.Text(root, height=1, width=4, padx=5)
+ca1=tk.Text(root, height=1, width=4, padx=5)        #mise en place des boutons de choix des dates
 ca1.place(x=1400, y=35)
 
 ca2=tk.Text(root, height=1, width=4, padx=5)
 ca2.place(x=1400, y=65)
 ca2.insert('1.0', str(auj.year))
 
-tj1=tk.Label(text="jour :", fg="blue", bg='grey')
+tj1=tk.Label(text="jour :", fg="blue", bg='grey')   
 tm1=tk.Label(text="mois :", fg="blue", bg='grey')
 ta1=tk.Label(text="année :", fg="blue", bg='grey')
 tj1.place(x=1110, y=35)
@@ -94,12 +73,12 @@ cj2.place(x=1150,y=65)
 cm2 = ttk.Combobox(root, values=mois, width=10)
 cm2.place(x=1250,y=65)
 cj2.current(auj.day-1)
-cm2.current(auj.month-1)
+cm2.current(auj.month-1)                #fin de mise en place des boutons de choix des dates
 
-zooms=tk.Text(root, height=1, width=2, padx=5)
+zooms=tk.Text(root, height=1, width=2, padx=5)      #bouton de zoom
 zooms.place(x=500, y=700)
 
-def getEntry():
+def getEntry():                         #lancement de la création de la figure après avoir entré les paramètres
     global ticker, dj, dm, da, fj, fm, fa
     ticker = text.get()
     dj = str(cj1.get())
@@ -110,13 +89,13 @@ def getEntry():
     fa = str(ca2.get("1.0",'end-1c'))
     triger(ticker, dj, dm, da, fj, fm, fa)
 
-def triger(ticker, dj, dm, da, fj, fm, fa):
+def triger(ticker, dj, dm, da, fj, fm, fa):     #séléction de la date de début et de fin
     global start, end
     start = da+'-'+dm+'-'+dj
     end = fa+'-'+fm+'-'+fj
     dessin()
   
-def dessin() :
+def dessin() :                                  #création du graph dans matplotlib
     global absi, start, end, ticker, plot1
     stock = yf.download(ticker, start, end)
     if not stock.empty :
@@ -124,69 +103,38 @@ def dessin() :
         plot1 = fig.add_subplot(111)
         y = stock.iloc[:,4]
         absi = stock.index[0:len(y)]
-        if not var1.get():
+        if not var1.get():                      #graph classique
 
-            # the figure that will contain the plot
-            
-            # list of squares
-            # adding the subplot
-
-            # plotting the graph
             plot1.plot(absi,y)
-        
-            # creating the Tkinter canvas
-            # containing the Matplotlib figure
-        else:
-              
-            # "up" dataframe will store the stock_prices 
-            # when the closing stock price is greater
-            # than or equal to the opening stock prices
+            
+        else:                                   #graph en chandelier
             up = stock[stock.Close >= stock.Open]
-              
-            # "down" dataframe will store the stock_prices
-            # when the closing stock price is
-            # lesser than the opening stock prices
             down = stock[stock.Close < stock.Open]
-              
-            # When the stock prices have decreased, then it
-            # will be represented by blue color candlestick
+            
             col1 = 'red'
-              
-            # When the stock prices have increased, then it 
-            # will be represented by green color candlestick
             col2 = 'green'
-              
-            # Setting width of candlestick elements
             width = 1
             width2 = .3
               
-            # Plotting up prices of the stock
             plot1.bar(up.index, up.Close-up.Open, width, bottom=up.Open, color=col1)
             plot1.bar(up.index, up.High-up.Close, width2, bottom=up.Close, color=col1)
             plot1.bar(up.index, up.Low-up.Open, width2, bottom=up.Open, color=col1)
               
-            # Plotting down prices of the stock
             plot1.bar(down.index, down.Close-down.Open, width, bottom=down.Open, color=col2)
             plot1.bar(down.index, down.High-down.Open, width2, bottom=down.Open, color=col2)
             plot1.bar(down.index, down.Low-down.Close, width2, bottom=down.Close, color=col2)
-              
-            # rotating the x-axis tick labels at 30degree 
-            # towards right
-              
-            # displaying candlestick chart of stock data 
-            # of a week
+            
         canvas = FigureCanvasTkAgg(fig, master = root)  
         canvas.draw()
         
-        # placing the canvas on the Tkinter window
         canvas.get_tk_widget().place(x=50,y=50)
         
-    if ticker not in historique :
+    if ticker not in historique :                   #enegistrement dans l'historique de la dernière entrée
         fichier = open("historique.txt", "a")
         fichier.write(ticker+' ')
         fichier.close()
         
-def export() :
+def export() :                      #exporter une image en pdf (à coriger)
     plot1.savefig("figure.jpeg", format="jpeg")
     pdf = FPDF()
     pdf.add_page()
@@ -194,7 +142,7 @@ def export() :
     pdf.output("doc-with-figure.pdf")
     os.remove("figure.jpeg")
     
-def browseFiles():
+def browseFiles():                  #ouvrir un fichier de sauvegarde
     global ticker
     filename = tk.filedialog.askopenfilename(filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
     fichier = open(filename, "r")
@@ -215,13 +163,13 @@ def browseFiles():
         ca1.insert('1.0', da)
         ca2.insert('1.0', fa)
 
-def save() :
+def save() :                        #sauvegarde des dates entrée et de l'entreprise choisi
     fichier = tk.filedialog.asksaveasfile(mode='w', filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
     ticker = text.get()
     fichier.write(ticker+' '+dj+' '+dm+' '+da+' '+fj+' '+fm+' '+fa)
     fichier.close()
 
-def clic(event):
+def clic(event):                    #zoom dans l'image à l'aide de la valeur entrée dans la case
     global Mem, absi, start, end
     zoom=int(zooms.get("1.0",'end-1c'))
     tx=len(absi)
@@ -247,20 +195,25 @@ def clic(event):
         else:
             dessin()
         
-def retour():
-    global Mem,start,end
-    ret=Mem.pop()
+def retour():                       #retour arrière du zoom
+    global Mem,start,end 
+    ret=Mem.pop()               
     start=ret[0]
     end=ret[1]
     dessin()
-    
+ 
+menubar = tk.Menu(root)                     #création de la barre d'outil
+menu1 = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Fichier", menu=menu1)
 menu1.add_command(label="Ouvrir", command=browseFiles)
 menu1.add_command(label="Sauvegarder", command=save)
 menu1.add_separator()
-menu1.add_command(label="Exporter en PDF", command=export)
+menu1.add_command(label="Exporter en PDF", command=export)  #fin de création de la barre d'outil
+root.config(menu=menubar)
+
 root.bind("<Button-1>",clic)
 
-btn = tk.Button(root, height=1, width=10, text="Lire", command=getEntry)
+btn = tk.Button(root, height=1, width=10, text="Lire", command=getEntry)  #création des boutons de lecture et de retour
 btn.place(x=50, y=20)
 rtn = tk.Button(root, height=1, width=10, text="Retour", command=retour)
 rtn.place(x=700,y=700)
