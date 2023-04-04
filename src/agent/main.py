2848b5f0a1b8f61b094f26f6d8961ee58101ce70
Henri NGO <henringo@Henri-NGO-5.local>
2023-04-04 23:17:16 +0200

    the whole idea is there but needs integration

--- src/agent/main.py
+++ src/agent/main.py
@@ -178,13 +178,14 @@ def one_draw_from_moving_block_bootstrap(rng, x, block_size):
     tmp[nr_draws * block_size:nr_draws * block_size + remain] = x[start:start + remain]
     return tmp
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------ Base de Functor -------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 import numpy as np
 import random
 from scipy.optimize import least_squares
-
-# TODO à replacer
-nr_prms = 4
-nr_stats = 7
 
 #Une classe qui implémente l'opérateur de la fonction appelable "()"
 class BaseFunctor:
@@ -201,11 +202,11 @@ class BaseFunctor:
 
 class Functor(BaseFunctor):
     def __init__(self, stats, print_=False):
-        super().__init__(nr_prms, nr_stats)
+        super().__init__(NR_PARAMS, NR_STATS)
         self.print_ = print_
         self.stats_ = stats
 
-    #La méthode call() est l'opérateur de la fonction appelable qui est appelé lorsque lorsque l'objet est utilisé comme une une fonction
+    #La méthode call() est l'opérateur de la fonction appelable qui est appelé lorsque lorsque l'objet est utilisé comme une fonction
     def __call__(self, beta, fvec):
         '''
         beta : vecteur de paramètres (tableau à 4 valeurs)
@@ -216,29 +217,40 @@ class Functor(BaseFunctor):
         beta_n = beta[1]
         beta_i = beta[2]
         lambda_ = beta[3]
-        assert nr_prms == 4
-        expr_threshold = 1
+        assert NR_PARAMS == 4
+        threshold = 1
 
         rng = np.random.default_rng()               #générateur de nombres
-        stats_cumul = np.zeros(nr_stats)           
+        rand_normal = rng.normal
+        rand_uniform = rng.uniform
+        #shock = np.array([rand_normal() for i in range(N)]) pas besoin car initialiser plus tard
+        expectations = np.array([rand_uniform() for i in range(N)])
 
-        for r in range(sim_R):                      
+        stats_cumul = np.zeros(NR_STATS)
+        dp = np.zeros(T)
+
+        for r in range(SIM_R):                      #le nombre de simulation
             dp_tm1 = 0
             trading_volume = 0
 
-            for t in range(begin_discard + T):
-                shock = rng.standard_normal(N)
-                expectations = np.power(1 + np.abs(dp_tm1), beta_m) * beta_i * shock
-
-                for exchange in range(nr_exchanges):
+            for t in range(BEGIN_DISCARD + T):
+                shock = rng.standard_normal(N)                                          #Un choc aléatoire est généré
+                expectations = np.power(1 + np.abs(dp_tm1), beta_m) * beta_i * shock    #Initialise des attentes sans connaître celle des voisins
+                
+                
+                for exchange in range(NR_EXCHANGES):
                     for i in range(N):
                         u = rng.uniform()
-                        j = random_neighbor(L, i, u)
+                        
+                        #Choix d'un voisin aléatoire
+                        j = west(L, i) if u < 0.25 else (south(L, i) if u < 0.5 else (east(L, i) if u < 0.75 else north(L, i)))
+                        #Actualise mes attentes en fonction des attentes des voisins
                         expectations[i] = np.power(1 + np.abs(dp_tm1), beta_m) * (beta_n * expectations[j] + beta_i * shock[i])
 
                 excess_demand = 0
                 local_trading_volume = 0
 
+                #Calcul de l'excès de demande sur chaque échange grâce aux attentes des traders
                 for i in range(N):
                     if expectations[i] < -threshold:
                         excess_demand -= 1
@@ -246,16 +258,18 @@ class Functor(BaseFunctor):
                         excess_demand += 1
                         local_trading_volume += 1
 
+                #Calcul du nouveau taux de change
                 dp_tm1 = excess_demand / lambda_
 
-                if t >= begin_discard:
-                    dp[t - begin_discard] = dp_tm1
-                    trading_volume += local_trading_volume
+                if t >= BEGIN_DISCARD:
+                    dp[t - BEGIN_DISCARD] = dp_tm1              #le taux de change est stocké dans le tableau 'dp'
+                    trading_volume += local_trading_volume      #le volume de transaction local est ajouté au volume de transaction globale
 
             if np.sum(dp) == 0:
                 print("None transactions...")
-                return 1
+                return 0
 
+            #différentes statistiques sont calculées à partir des taux de change stockés dans dp
             centered_dp = centered(dp)
             stats_cumul[0] += compute_sigma(centered_dp)
             stats_cumul[1] += compute_kurtosis(centered_dp)
@@ -264,54 +278,47 @@ class Functor(BaseFunctor):
             stats_cumul[4] += compute_autocorrelation_squares(dp, 1)
             stats_cumul[5] += compute_autocorrelation_squares(dp, 10)
             stats_cumul[6] += trading_volume / N / T
-            assert nr_stats == 7
+            assert NR_STATS == 7
 
-        for j in range(nr_stats):
-            fvec[j] = stats_cumul[j] / sim_R - self.stats_[j]
+        
+        # TODO à modifier
+        for j in range(NR_STATS):
+            fvec[j] = stats_cumul[j] / SIM_R - self.stats_[j]
 
-        if self.print_:
-            print("_m\t_n\t_i\t\t\t\t_0\t_10\t_0\t_10\ttv\tfit")
-            for j in range(nr_prms):
-                print(beta[j], end='\t')
-            for j in range(nr_stats):
-                print(stats_cumul[j] / sim_R, end='\t')
-            print(np.sum((stats_cumul / sim_R - self.stats_) ** 2))
-
-        return 0
+        return 1
 
 import numpy as np
 from scipy.optimize import least_squares
+import scipy.optimize
 import threading
 import os
 
+#Cette classe centralise toutes les informations que l'on a besoin pour lancer l'estimation
 class Estimation:
     def __init__(self, r, M, beta0):
-        self.r = r
-        self.stats = M[r, :].T
-        self.beta = beta0.copy()
+        self.r = r                                      #Le nombre de tâche
+        self.stats = np.transpose(M[:r, :])             #Des statistiques à reproduire, chaque estimation essaye de reproduire des paramètres statistiques
+        self.beta = beta0                               #le point de départ qui deviendra la solution
         print(f"{r} constructed...")
 
     def estimation_task(self):
         print(f"Task {self.r} starts...")
+        functor = lambda x: (self.stats @ x).reshape((-1, 1))                   #functor est une fonction qui calcule le produit scalaire entre self.stats et x renvoie grâce au reshape(-1,-1) autant de colonne que d'entrée et automatique le nb de ligne
+        numDiff = lambda x: np.gradient(functor(x), axis=0) / 0.0001            #fonction qui calcule le gradient de la fonction functor avec un pas de 0.0001, axis=0 derivée par rapport à la première dérivée de la matrice
         
-        def functor(beta):
-            # Define the function to compute the difference between the estimated and target statistics.
-            # You'll need to replace this with the actual function based on your problem.
-            return np.zeros_like(self.stats)
-
-        result = least_squares(functor, self.beta)
-        print(f"Task {self.r} completes...")
+        #Pour minimiser une fonction de plusieurs variables en trouvant des variables qui minimisent la fonction
+        #functor est la fonction a minimiser, 
+        #self.beta sont les valeurs initiales des variables
+        lm = scipy.optimize.least_squares(functor, self.beta, jac=numDiff)
+        print(f"Task {self.r} completes...")                              
         
-        # The results are put at the end of a file, one file per thread.
-        # The name of the file is built with the id of the thread.
-        file_name = f"{threading.get_ident()}.txt"
-        with open(file_name, "a") as out:
-            out.write(f"{self.r}\t{result.status}\t")
-            for j in range(len(self.stats)):
-                out.write(f"{self.stats[j]}\t")
-            for j in range(len(self.beta)):
-                out.write(f"{self.beta[j]}\t")
-            out.write("\n")
+        
+        # Write results to file
+        filename = f"{threading.get_ident()}.txt"
+        with open(filename, "a") as f:
+            f.write(f"{self.r}\t{lm.status}\t")
+            f.write("\t".join(map(str, self.stats.flatten())) + "\t")
+            f.write("\t".join(map(str, lm.x.flatten())) + "\n")
 
 import numpy as np
 import os
@@ -320,36 +327,14 @@ from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 
 def main():
-    # Replace the following functions with the appropriate implementations
-    def get_returns(file_name):
-        pass
-
-    def one_draw_from_moving_block_bootstrap(rng, returns, block_size):
-        pass
-
-    def centered(x):
-        pass
-
-    def compute_sigma(centered_x):
-        pass
-
-    def compute_kurtosis(centered_x):
-        pass
-
-    def compute_autocorrelation(centered_x, lag):
-        pass
-
-    def compute_autocorrelation_squares(x, lag):
-        pass
 
     rng = np.random.default_rng()
     returns = get_returns("CAC40-cours-journalier-ôclture.txt")
     R = 100  # Replace with the correct value for R
-    nr_stats = 7
-    M = np.zeros((R, nr_stats))
+    M = np.zeros((R, NR_STATS))
 
     for r in range(R):
-        x = returns if r == 0 else one_draw_from_moving_block_bootstrap(rng, returns, block_size)
+        x = returns if r == 0 else one_draw_from_moving_block_bootstrap(rng, returns, BLOCK_SIZE)
         centered_x = centered(x)
         M[r, 0] = compute_sigma(centered_x)
         M[r, 1] = compute_kurtosis(centered_x)
@@ -359,20 +344,23 @@ def main():
         M[r, 5] = compute_autocorrelation_squares(x, 10)
         M[r, 6] = 0.01
 
-    nr_prms = 4
     beta = np.array([1.73058, 1.04878, 0.0651077, 1603.94])
     stats = M[0, :]
-    # Replace the following line with the actual optimization step
-    # based on your problem
-    ret, beta = 0, beta
 
+    # TODO initialisation de ret avec estimation_task ?
+    
+    
+    #vecteur de taille R pour chaque simulation
     estimations = [Estimation(r, M, beta) for r in range(R)]
 
+    #Creation une pool of threads
     with ThreadPoolExecutor() as executor:
         for r in range(1, R):
             executor.submit(estimations[r].estimation_task)
+    #Toutes les tâches ont été exécuté
 
-    for j in range(nr_prms):
+    #Pour chaque paramètre, il calcule la moyenne, le 5%, la médiane, le 95% et le pourcentage
+    for j in range(NR_PARAMS):
         tmp = np.array([estimations[r].beta[j] for r in range(R)])
         tmp.sort()
         print(j, tmp.mean(), tmp[int(0.05 * R)], tmp[int(0.5 * R)], tmp[int(0.95 * R)])
