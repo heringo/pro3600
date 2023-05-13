7445a36b61d2bbc6e505d59c720be081156ecb0f
Theo92100 <121232962+Theo92100@users.noreply.github.com>
2023-05-13 09:49:53 +0200

    Add files via upload

--- src/NeuralProphet/Neural++.py
+++ src/NeuralProphet/Neural++.py
@@ -13,43 +13,38 @@ import plotly_resampler
 # Override the pandas_datareader
 yf.pdr_override()
 
-#Daily Forecasting
-#df = pdr.get_data_yahoo('AAPL', start='2010-01-01',end='2023-05-08')
-#df=df.reset_index(drop=False)
-#df=df[['Date','Adj Close']]
-#df['Date']=pd.to_datetime(df['Date'])
-#df=df.rename(columns={'Date':'ds','Adj Close':'y'})
-#m=NeuralProphet()
-#m.fit(df, freq="D")
-#future=m.make_future_dataframe(df, periods=60, n_historic_predictions=len(df))
-#forecast=m.predict(future)
-#fig=m.plot(forecast)
-#fig.show()
 
-#Minute Forecasting
-#set the appropriate forecast period
-forecast_period=60 
-# Set the end date as today's date
+##Minute Forecasting
+
+#Set the appropriate forecast period
+forecast_period=60
+number_of_days=1
 
  # Set the end date as today's date
 end_date = datetime.datetime.now().strftime("%m/%d/%Y")
+ 
  # Set the start date as 7 days before the end date
-start_date = (datetime.datetime.now() - datetime.timedelta(days=7)).strftime("%m/%d/%Y")
+start_date = (datetime.datetime.now() - datetime.timedelta(days=number_of_days)).strftime("%m/%d/%Y")
+
  # Get the minute data for a specific stock for the last 7 days
-stock_data = si.get_data("AAPL", start_date=start_date, end_date=end_date, interval="1m")
+stock_data = si.get_data("SOI.PA", start_date=start_date, end_date=end_date, interval="1m")
+ 
  # Reformat the data to only include the Date and Close columns
 stock_data = stock_data.reset_index(drop=False)
-
 stock_data = stock_data[['index', 'close']]
+
 # Rename the columns to match the Prophet API
 stock_data = stock_data.rename(columns={'index': 'ds', 'close': 'y'})
+
 # Convert the Date column to a datetime type
 stock_data['ds'] = pd.to_datetime(stock_data['ds'])
+
 #Filter the data
 date=stock_data['ds'].max()-datetime.timedelta(minutes=forecast_period)
 stock_data1 = stock_data.loc[stock_data['ds'] < date, ['ds', 'y']]
+
 #Initialize the model and fit the data
-m=NeuralProphet(changepoints_range=0.90,seasonality_mode='multiplicative',daily_seasonality=4,ar_reg=0.25,num_hidden_layers=3,d_hidden=64)
+m=NeuralProphet(changepoints_range=0.95,ar_reg=0.5,num_hidden_layers=5,d_hidden=64)
 m.fit(stock_data1, freq="min")
 future=m.make_future_dataframe(stock_data1, periods=forecast_period, n_historic_predictions=len(stock_data))
 forecast=m.predict(future)
@@ -65,27 +60,9 @@ def forecastdates(forecast):
     forecast= pd.to_datetime(forecast)
     forecast=forecast.strftime("%Y-%m-%dT%H:%M:%S")
     return forecast
-#print(forecastdates(forecast))
-#print(forecastvalues(forecast))
 actuals = stock_data.set_index('ds').iloc[-len(forecast):]['y'].reset_index()
 actuals['y'] = actuals['y'].interpolate()
 fig = m.plot(forecast)
 fig.update_traces(name='Forecast', showlegend=True)
 fig.add_scatter(x=actuals['ds'], y=actuals['y'], mode='lines', name='Actuals')
 fig.show()
-
-#Write the forecast values and dates to txt files
-
-#def write_forecastvalues_to_txt(forecast, file_path):
-  
-    #with open(file_path, 'w') as f:
-        #for val in forecast:
-            #f.write(f"{val}\n")
-#write_forecastvalues_to_txt(forecastvalues(forecast), './ProjetInfo/pro3600/forecast.txt')
-
-#def write_forecastdates_to_txt(forecast, file_path):
-      
-     #with open(file_path, 'w') as f:
-          #for val in forecast:
-                #f.write(f"{val}\n")
-#write_forecastdates_to_txt(forecastdates(forecast), './ProjetInfo/pro3600/forecastdates.txt')
--- src/NeuralProphet/Neural++Daily.py
+++ src/NeuralProphet/Neural++Daily.py
@@ -6,55 +6,78 @@ import yfinance as yf
 import matplotlib.pyplot as plt 
 import plotly
 from pandas_datareader import data as pdr
-import yahoo_fin.stock_info as si
 import datetime
 import plotly_resampler
 from dateutil.relativedelta import relativedelta
+
+ # Get the current date in the desired format
 def get_current_date():
-    # Get the current date in the desired format
     return datetime.date.today().strftime('%Y-%m-%d')
 
+#Function to obtain the start date by substracting a number of years
 def subtract_years(date_str, years):
-    # Convert the date string to a datetime object
     date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
-    # Subtract the specified number of years from the date object
     new_date_obj = date_obj - datetime.timedelta(days=365*years)
-    # Convert the new date object back to a string and return it
     return new_date_obj.strftime('%Y-%m-%d')
+
 # Override the pandas_datareader
 yf.pdr_override()
-#Daily Forecasting
+
+##Daily Forecasting
+
 #Set the predefined variables
-forecast_period=60
+forecast_period=120
 number_of_years=10
 
 # Set the end date as today's date
 end_date = get_current_date()
 ten_years_ago = subtract_years(end_date, 10)
+
 # Set the start date as 10 years before the end date
 start_date = ten_years_ago
-print(start_date)
-df = pdr.get_data_yahoo('AAPL', start=start_date,end=end_date)
+
+#Get the data
+df = pdr.get_data_yahoo('AMZN', start=start_date,end=end_date)
+
+#Preprocess the data
 df=df.reset_index(drop=False)
 df=df[['Date','Adj Close']]
 df['Date']=pd.to_datetime(df['Date'])
+
+#Renaming the columns for the NeuralProphet model
 df=df.rename(columns={'Date':'ds','Adj Close':'y'})
 df['ds']=pd.to_datetime(df['ds'])
+
+#Fill in the missing values with the linear interpolation method
 df['y']=df['y'].interpolate()
-m=NeuralProphet(changepoints_range=0.8,n_changepoints=5,ar_reg=1,num_hidden_layers=3,d_hidden=64)
+
+#Instantiate the model with parameters
+m=NeuralProphet(changepoints_range=0.95,n_changepoints=30,batch_size=128,epochs=150)
+
+#Fitting the model
 m.fit(df, freq="B")
-future=m.make_future_dataframe(df, periods=60, n_historic_predictions=len(df))
+
+#Modify the data using .makefuture_dataframe to get the new dates with +120 days
+future=m.make_future_dataframe(df, periods=forecast_period, n_historic_predictions=len(df))
+
+#Predict on the future data
 forecast=m.predict(future)
+
+#Plotting the data
 fig=m.plot(forecast)
 fig.show()
+
+#Defining a function to get the predicted values of the forecast and put them in a list as strings
 def forecastvalues(forecast):
     forecast = forecast['yhat1'].values
     return forecast
+
+#Defining a function to get the predicted dates of the forecast and put them in a list as strings
 def forecastdates(forecast):
     forecast = forecast['ds'].values
     forecast= pd.to_datetime(forecast)
     forecast=forecast.strftime("%Y-%m-%d")
     return forecast
-#print(forecastdates(forecast))
-#print(forecastvalues(forecast))
+forecastdates(forecast)
+forecastvalues(forecast)
 
