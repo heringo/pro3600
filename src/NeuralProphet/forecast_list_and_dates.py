a8e2cd905537514fb3fcd334dbaa17445cadb3e9
Theo92100 <121232962+Theo92100@users.noreply.github.com>
2023-05-15 19:44:11 +0200

    modified cleaned code

--- src/NeuralProphet/forecast_list_and_dates.py
+++ src/NeuralProphet/forecast_list_and_dates.py
@@ -1,6 +1,5 @@
 
 import pandas as pd
-from neuralprophet import NeuralProphet
 def forecastvalues(forecast : pd.DataFrame):
 
     """This function takes the forecast dataframe from NeuralProphet( yhat1 is the prediction column) and returns the predicted values of the forecast and put them in a list as strings
