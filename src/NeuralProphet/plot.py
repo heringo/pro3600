a609ab881238e3af474cd12c85d4f01d29ed0ca2
Theo92100 <121232962+Theo92100@users.noreply.github.com>
2023-05-22 11:12:15 +0200

    Add files via upload
    modified

--- src/NeuralProphet/plot.py
+++ src/NeuralProphet/plot.py
@@ -1,7 +1,7 @@
-import plotly
+import plotly.express as px
 from neuralprophet import NeuralProphet
 import pandas as pd
-def plotting(m : NeuralProphet,forecast : pd.DataFrame):
+def plotting(m : NeuralProphet,forecast : pd.DataFrame,forecast_period : int = 60):
     """This function takes the model from NeuralProphet and the forecast and uses NeuralProphetplot to create a figure which will be plotted in main
 
     Args: m (NeuralProphet) the NeuralProphet model instantiated
@@ -10,7 +10,12 @@ def plotting(m : NeuralProphet,forecast : pd.DataFrame):
     Returns: fig (Figure : Any) The figure from plotly which will be plotted
     """
     
-    fig=m.plot(forecast)
-    
-    
+    yhat=pd.concat([forecast['yhat1'][:-forecast_period],forecast[f'yhat{forecast_period}'].tail(forecast_period)],ignore_index=True)
+    yhat=yhat.reset_index(drop=True)
+    yhat=yhat.interpolate()
+
+    forecast_ds_reset = forecast['ds'].reset_index(drop=True)
+
+    result = pd.DataFrame({'ds': forecast_ds_reset, 'yhat': yhat})
+    fig=px.line(result,x='ds',y='yhat')
     return fig
