fbe681ee5e46290dc112dbfb45c855d978196a95
valentinsix <124298748+valentinsix@users.noreply.github.com>
2023-05-22 11:08:22 +0200

    Add files via upload

--- src/wwwroot/forecast_list_and_dates.py
+++ src/wwwroot/forecast_list_and_dates.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
-def forecastvalues(forecast : pd.DataFrame):
+from neuralprophet import NeuralProphet
+def forecastvalues(forecast : pd.DataFrame,forecast_period : int = 60):
 
     """This function takes the forecast dataframe from NeuralProphet( yhat1 is the prediction column) and returns the predicted values of the forecast and put them in a list as strings
     
@@ -8,11 +9,17 @@ def forecastvalues(forecast : pd.DataFrame):
     
     Returns: The predicted values  of the forecast as a list of strings
     """
+    yhat = pd.concat([forecast['yhat1'][:-forecast_period],forecast[f'yhat{forecast_period}'].tail(forecast_period)], ignore_index=True)
+    yhat=yhat.reset_index(drop=True)
+    yhat=yhat.interpolate()
+
+    forecast_ds_reset = forecast['ds'].reset_index(drop=True)
+
+    result = pd.DataFrame({'ds': forecast_ds_reset, 'yhat': yhat})
+    result =result['yhat'].values
     
-    forecast = forecast['yhat1'].values
     
-    
-    return forecast
+    return result
 
 def forecastdates(forecast : pd.DataFrame):
     """This function takes the forecast dataframe from NeuralProphet( ds is the date column) and returns the predicted dates of the forecast and put them in a list as strings
